d7bf335572ef72ba8589130ce3e90aa7894c6a60
rosedive <sakindiuweramarie@gmail.com>
2019-11-25 11:07:50 -0800

    make validation

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,13 @@
 class Property < ApplicationRecord
-end
+    
+    
+    validate :diff
+    
+    def diff
+        if minutes_walk1 <= minutes_work2
+            errors.add(:How_many_minutes_walk, "please check again how it works")
+        # elsif minut = minut1
+        #     errors.add(:How_many_minutes_walk, "must be different")
+            end
+            end
+        end
